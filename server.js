cd248583afb99e51d9057d017585102fe5862599
Krisha Elle <krisharelle@gmail.com>
2025-04-28 00:48:14 -0400

    server.js fixes

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 HermesPass server running on https://beamish-baklava-7a2363.netlify.app/:${PORT}`);
+    console.log(`🚀 HermesPass server running on ${PORT}`);
 });
